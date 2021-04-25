6444df50e99af7cffebf609f84bb4fcb73071860
SprengerV <b.francis@sprengerv.com>
2021-04-24 20:17:28 -0400

    added header/navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <header>
-      
+    <header className="navbar navbar-expand-lg navbar-light bg-danger d-flex justify-content-center">
+      <a href="/" className="navbar-brand ms-auto me-auto">
+        Employee Directory
+      </a>
     </header>
   );
 }
+
+export default Navbar;
