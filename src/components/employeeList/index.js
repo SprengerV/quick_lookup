7f7e260d1fa2010a4a3fa92baee2140d9ea672bb
SprengerV <b.francis@sprengerv.com>
2021-04-25 13:57:10 -0400

    asc/desc button switch

--- src/components/employeeList/index.js
+++ src/components/employeeList/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListHeader from '../listHeader'
 
-function EmployeeList() {
-    return(
-        <div>
-            <ListHeader />
-            <h1>EMPLOYEES</h1>
-        </div>
-    );
+class EmployeeList extends Component {
+    state = {
+        nameSort: ""
+    };
+
+    nameSortHandler = () => {
+        switch (this.state.nameSort) {
+            case "asc":
+              this.setState({ nameSort: "desc" });
+              break;
+            case "desc":
+              this.setState({ nameSort: "asc" });
+              break;
+            default:
+              this.setState({ nameSort: "asc" });
+        }
+    }
+    render() {
+        return(
+            <div>
+                <ListHeader nameSort={this.state.nameSort} nameSortHandler={this.nameSortHandler}/>
+                <h1>EMPLOYEES</h1>
+            </div>
+        );
+    }
 }
 
 export default EmployeeList;
