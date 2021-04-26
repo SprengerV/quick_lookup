ea9d6ba01728d8472128903de1642b4239a3604f
SprengerV <b.francis@sprengerv.com>
2021-04-26 14:07:00 -0400

    sorting functionality

--- src/components/employeeList/index.js
+++ src/components/employeeList/index.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import ListHeader from '../listHeader';
 import Employee from '../employee';
-import { isThisTypeNode } from 'typescript';
 
 class EmployeeList extends Component {
     state = {
-        nameSort: ""
+        nameSort: "",
+        sorted: []
     };
 
+    componentDidMount() {
+        this.setState({ sorted: this.props.employees });
+    }
     nameSortHandler = () => {
+        const emps = this.props.employees;
         switch (this.state.nameSort) {
-            case "asc":
-              this.setState({ nameSort: "desc" });
-              break;
+            case "asc": 
+                this.setState({
+                    nameSort: "desc",
+                    sorted: emps.sort((a, b) => b.name.last.localeCompare(a.name.last, 'en', { 'sensitivity': 'base' }))
+                });
+                break;
             case "desc":
-              this.setState({ nameSort: "asc" });
-              break;
+                this.setState({
+                    nameSort: "asc",
+                    sorted: emps.sort((a, b) => a.name.last.localeCompare(b.name.last, 'en', { 'sensitivity': 'base' }))
+                });
+                break;
             default:
-              this.setState({ nameSort: "asc" });
+                this.setState({
+                    nameSort: "asc",
+                    sorted: emps.sort((a, b) => a.name.last.localeCompare(b.name.last, 'en', { 'sensitivity': 'base' }))
+                });
         }
     }
     render() {
         return(
             <div>
                 <ListHeader nameSort={this.state.nameSort} nameSortHandler={this.nameSortHandler}/>
-                {this.props.employees.map((employee, i) => (
+                {this.state.sorted.map((employee, i) => (
                     <Employee key={i} index={i} data={employee}/>
                 ))}
             </div>
